9cba412c8e96213f756ae9631f60936d49b27bb7
Pat <mrpatblack@gmail.com>
2019-06-08 17:43:52 -0500

    heroku depolyment

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 var mysql = require('mysql');
 
-
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
 	host: 'localhost',
 	port: 8889,
 	user: 'root',
 	password: 'root',
 	database: 'burgers_db'
 });
+
+};
+
 
 
 connection.connect(function(err) {
